ad73f43e1da8bb803609dbc9192870ff9328131b
Jin Cho <jinhyuki@gmail.com>
2016-06-19 23:26:44 -0700

    for github page deployment

--- bin/promiser.sh
+++ bin/promiser.sh
@@ -5,6 +5,8 @@ elif [ "$1" = "serve" ]; then
     (cd ../; sproutcore server --allow-from-ips='*.*.*.*')
 elif [ "$1" = "build" ]; then
     (cd ../; sproutcore build --buildroot=./build --languages=en)
+    (cd ../; find ./build/ -type f -name '*.js' -o -name '*.css' -o -name '*.html' -exec sed -i '' -e 's/\/static\/sproutcore/\/promiser\/build\/static\/sproutcore/g' {} \;)
+    (cd ../; find ./build/ -type f -name '*.js' -o -name '*.css' -o -name '*.html' -exec sed -i '' -e 's/\/static\/promiser/\/promiser\/build\/static\/promiser/g' {} \;)
 else
     echo "Usage:"
     echo "- serve: Serve"
